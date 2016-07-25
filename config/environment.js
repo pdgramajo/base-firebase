040ea0eeadf60f7d12b336e0eef098369e4c253c
pdgramajo <pdp1985gramajo@gmail.com>
2016-07-25 13:19:26 -0300

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -6,7 +6,7 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
-    firebase: 'https://bdember.firebaseio.com/',
+    firebase: 'https://YOUR_APP_NAME.firebaseio.com/',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
