d2388db2e52109a356d3bf0c332b5919f621a9a5
Pablo Gramajo <pgramajo@jbknowledge.com>
2016-07-25 13:12:26 -0300

    primer commit

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,7 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    firebase: 'https://bdember.firebaseio.com/',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -16,6 +17,9 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+    contentSecurityPolicy: {
+        'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com"
     }
   };
 
